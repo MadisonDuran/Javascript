0b44c4abd5eb68f07a206a7ce2a072240eda146e
Madison Duran <madisonduran74@gmail.com>
2025-07-28 11:57:24 -0400

    added comments to my code.

--- 1.Easy.Function/script.js
+++ 1.Easy.Function/script.js
@@ -1,15 +1,17 @@
-// Javascript Challenge
-
 // 1. EASY 
+// This function uses closure to return a function that logs today's exercise.
 
 function exercise(exerciseName) {
+    // The inner function remembers the exerciseName parameter
     return function() {
+    // Log the message dynamically with the exercise name
         console.log("Today's Exercise: " + exerciseName);
     }
 }
 
-exercise("Running")();
-exercise("Swimming")();
-exercise("Dancing")();
-exercise("Fencing")();
+exercise("Running")(); // Output: Today's Exercise: Running
+exercise("Swimming")(); // Output: Today's Exercise: Swimming
+exercise("Dancing")(); // Output: Today's Exercise: Dancing
+exercise("Fencing")(); // Output: Today's Exercise: Fencing
 
+git 
